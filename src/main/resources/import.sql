2f165c2ef22821885b2d1c4e24aa1407521a7fef
vojta udrzal <udrzalv@gmail.com>
2016-04-19 13:40:51 +0200

    fixed truncate

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -63,6 +63,6 @@ INSERT INTO expirations (itemtype, expirationdays) VALUES ('TURKEY', 80);
 INSERT INTO expirations (itemtype, expirationdays) VALUES ('TURTLE', 71);
 INSERT INTO expirations (itemtype, expirationdays) VALUES ('VEAL', 21);
 
-TRUNCATE expirations CASCADE;
+TRUNCATE config CASCADE;
 INSERT INTO config(id, date) VALUES (1, '2016-04-19 00:00:00.0');
 
