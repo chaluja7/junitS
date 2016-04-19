1b4af6eb3a7a117ad5c4e1d4ff63d3af18181cd4
Petr Marek <marekp11@fel.cvut.cz>
2016-04-19 14:03:23 +0200

    reportservice

--- src/main/java/cz/cvut/junit/service/ReportServiceImpl.java
+++ src/main/java/cz/cvut/junit/service/ReportServiceImpl.java
@@ -1,12 +1,15 @@
 package cz.cvut.junit.service;
 
+import cz.cvut.junit.entity.Item;
 import cz.cvut.junit.entity.ItemShelfConnection;
 import cz.cvut.junit.pojo.ReportItem;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.ReadableInstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by sange on 19/04/16.
@@ -20,11 +23,48 @@ public class ReportServiceImpl implements ReportService{
     @Autowired
     private ItemShelfConnectionService itemShelfConnectionService;
 
+    @Autowired
+    private ItemService itemService;
+
+    private List<ReportItem> reportItems;
+
     @Override
     public List<ReportItem> findAll() {
-        HashMap<String, Integer> expirationServiceAll = expirationService.findAll();
+        HashMap<String, Integer> expirations = expirationService.findAll();
         List<ItemShelfConnection> itemShelfConnections = itemShelfConnectionService.findAll();
+        List<Item> items = itemService.findAll();
+        
+
+        if(reportItems == null){
+            reportItems = new ArrayList<>();
+        }
+
+        for (Item item : items) {
+            for (ItemShelfConnection isc : itemShelfConnections) {
+                for (Map.Entry<String, Integer> e : expirations.entrySet()) {
+                    String type = e.getKey();
+                    int expirationDays = e.getValue();
+                    if(isc.getItem() == item && Objects.equals(item.getType(), type)){
+
+                        ReportItem reportItem = new ReportItem();
+                        reportItem.setType(type);
+                        reportItem.setCount(isc.getCount());
+                        reportItem.setFrozen(item.getFrozen());
+
+                        DateTime now = DateTime.now();
+                        DateTime killTime = new DateTime(item.getKillDate());
+
+                        int expiresInDays = Days.daysBetween(killTime.plusDays(expirationDays), now).getDays();;
+                        reportItem.setExpiresInDays(expiresInDays);
+
+                        reportItems.add(reportItem);
+                    }
+
+                }
+            }
+        }
 
         return null;
     }
+
 }
